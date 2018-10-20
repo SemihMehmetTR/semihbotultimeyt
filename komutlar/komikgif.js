27186575c239eadc373c8a281ee60f3fd0bda59d
SemihMehmetTR <44204684+SemihMehmetTR@users.noreply.github.com>
2018-10-20 16:04:55 +0300

    Update and rename komutlar/komikgif.js to  komutlar/komikgif.js

--- komutlar/komikgif.js
+++ komutlar/komikgif.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let replies = ["https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+    let replies = ["https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif", "https://media.giphy.com/media/ASzK5wWjMtc6A/giphy.gif", "https://www.speakgif.com/wp-content/uploads/2016/02/donald-trump-funny-face-animated-gif.gif", "http://bestanimations.com/Animals/Birds/Chickens/funny-chicken-beats-cat-gif.gif", "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif", "https://media.giphy.com/media/1wqqlaQ7IX3TXibXZE/giphy.gif", "https://media.giphy.com/media/26tP3M3i03hoIYL6M/giphy.gif", "https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif", "https://media.giphy.com/media/11StaZ9Lj74oCY/giphy.gif", "https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif"];
 
     let result = Math.floor((Math.random() * replies.length));
 
